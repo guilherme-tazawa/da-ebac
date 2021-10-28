71b6d7c6d82fbbe6a6d60addc8410a9b1ebfdc0a
guilherme-tazawa <guilherme.tazawa@gmail.com>
2021-10-28 19:43:58 +0000

    arquivo gasolina.py e gasolina.png alterado em develop

--- gasolina.py
+++ gasolina.py
@@ -5,5 +5,5 @@ gasolina_df = pd.read_csv('gasolina.csv', sep=',')
 
 with sns.axes_style('whitegrid'):
   grafico = sns.lineplot(data=gasolina_df, x="dia", y="venda", palette="pastel")
-  grafico.set(title='Preço médio da gasolina - SP', xlabel='Dia', ylabel='Preço')
+  grafico.set(title='Preço médio da gasolina em julho', xlabel='Dias', ylabel='Preço médio')
   grafico.figure.savefig(fname="gasolina.png", bbox_inches="tight");
